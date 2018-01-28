72130044d1f1d86a664f460e5ea5e08e2f296592
Joseph Weidinger <jsphweid@gmail.com>
2018-01-28 06:10:31 -0800

    use bash to deploy

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+bucket_name=$(<~/.aws/main_bucket_name)
+cloudfront_id=$(<~/.aws/main_cloudfront_id)
+
+aws s3 rm s3://${bucket_name}/ --recursive --exclude "*/*"
+aws s3 cp ./dist/ s3://${bucket_name}/ --recursive
+aws cloudfront create-invalidation --distribution-id ${cloudfront_id} --paths "/*"
