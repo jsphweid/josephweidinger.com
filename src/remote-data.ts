a6f79f7fa507e0049f9fa07f9d87934e182e7f1e
joseph.weidinger <joseph.weidinger@equipmentshare.com>
2021-05-22 10:51:10 -0500

    add some degree of caching, even if in a very manual way

--- src/remote-data.ts
+++ src/remote-data.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
+import lsCache from "lscache";
+
 import * as Utils from "./utils";
+
+export interface RawActivityLog {
+  vendorType: string;
+  date: string;
+  basicHtml: string;
+}
 
 export interface ActivityLog {
   vendorType: string;
@@ -13,7 +21,47 @@ interface ActivityLogsRequest {
   first?: number;
 }
 
-export const getActivityLogs = (
+const mapRawActivityLog = (rawActivityLog: RawActivityLog): ActivityLog => ({
+  ...rawActivityLog,
+  date: new Date(rawActivityLog.date)
+});
+
+// could've used react-static cache if I didn't misuse it I think, but it's easy
+// enough just to quickly write my own...
+// NOTE: where this breaks down is that each day that passes, a new set of start/end
+// keys are made for previous days, which isn't 100% ideal. Maybe a better way
+// would be to have a static past date fetching algorithm such that it always
+// looks for the same dates/keys. This will do for now, however...
+class ActivityLogsCache {
+  private prefix: string;
+  constructor(prefix: string) {
+    this.prefix = prefix;
+  }
+
+  private makeCacheKey = (start: Date, end: Date): string =>
+    `${this.prefix}${start.toISOString()}-${end.toISOString()}`;
+
+  get = (req: ActivityLogsRequest): ActivityLog[] | null => {
+    if (req.start && req.end) {
+      const key = this.makeCacheKey(req.start, req.end);
+      const maybeItem = lsCache.get(key);
+      return maybeItem ? JSON.parse(maybeItem).map(mapRawActivityLog) : null;
+    }
+    return null;
+  };
+
+  set = (req: ActivityLogsRequest, data: ActivityLog[]): void => {
+    if (req.start && req.end) {
+      const key = this.makeCacheKey(req.start, req.end);
+      const minutesToExpire = 60 * 24 * 3; // 3 days
+      lsCache.set(key, JSON.stringify(data), minutesToExpire);
+    }
+  };
+}
+
+const activityLogsCache = new ActivityLogsCache("activityLogs");
+
+export const getActivityLogs = async (
   req: ActivityLogsRequest = {}
 ): Promise<ActivityLog[]> => {
   // TODO: clean all this bullshit up (use lib?)
@@ -22,9 +70,18 @@ export const getActivityLogs = (
   ).toISOString()}`;
   const endDateString = req.end ? `&end=${req.end.toISOString()}` : "";
   const firstString = req.first ? `&first=${req.first}` : "";
-  return axios
-    .get<ActivityLog[]>(
+  const cacheResult = activityLogsCache.get(req);
+
+  if (cacheResult) {
+    return Promise.resolve(cacheResult);
+  }
+
+  const result = await axios
+    .get<RawActivityLog[]>(
       `https://rd2o5zpumc.execute-api.us-west-2.amazonaws.com/prod/logs${startDateString}${endDateString}${firstString}`
     )
-    .then(r => r.data.map(item => ({ ...item, date: new Date(item.date) })));
+    .then(r => r.data.map(mapRawActivityLog));
+
+  activityLogsCache.set(req, result);
+  return result;
 };
